2374c3987aaedbd683583d0c10b66a961d91987c
Adria Navarro <anavarromartin@pivotal.io>
2019-05-15 10:50:08 -0500

    find color in border

--- src/color-search.js
+++ src/color-search.js
@@ -9,13 +9,20 @@ const findColorInstancesAcrossAllPages = colorToFind => {
 const findColorInLayers = (colorToFind, layers, result) => {
     return layers
         .flatMap(layer => {
-            if (layer.style && layer.style.fills.length > 0) {
-                if (layer.style.fills[0].color.toUpperCase() === colorToFind.toUpperCase()) {
-                    return {
-                        layer: layer,
-                        ...result
-                    }
-                }
+            if (layer.style
+                && (
+                    (layer.style.fills
+                        && layer.style.fills.length > 0
+                        && layer.style.fills[0].color.toUpperCase() === colorToFind.toUpperCase())
+                    || (layer.style.borders
+                        && layer.style.borders.length > 0
+                        && layer.style.borders[0].color.toUpperCase() === colorToFind.toUpperCase())
+                )
+            ) {
+                return {
+                    layer: layer,
+                    ...result
+                };
             }
 
             if (layer.layers && layer.layers.length > 0) {
--- test/color-locator.spec.js
+++ test/color-locator.spec.js
@@ -1,4 +1,4 @@
-import { findColorInstancesInPage } from "../src/color-search";
+import {findColorInstancesInPage} from "../src/color-search";
 
 const page = {
     name: 'Page 1',
@@ -10,14 +10,15 @@ const page = {
                 {
                     name: "first sub layer",
                     style: {
-                      fills: [{color: 'NOT THE COLOR'}]
+                        fills: [{color: 'NOT THE COLOR'}]
                     },
                     layers: [
                         {
                             name: 'second sub layer',
                             parent: {name: 'first sub layer'},
                             style: {
-                                fills: [{color: 'COLOR'}]
+                                fills: [{color: 'COLOR'}],
+                                borders: [{color: 'COLOR'}]
                             }
                         },
                         {
@@ -33,21 +34,50 @@ const page = {
     ]
 };
 
-describe('when searching on a page', () => {
-    let results;
-    beforeAll(() => {
-        results = findColorInstancesInPage('COLOR', page);
+const pageWithBorderColor = {
+    name: 'page',
+    layers: [
+        {
+            name: 'artboard',
+            type: 'Artboard',
+            layers: [{
+                name: 'layer with border',
+                style: {
+                    borders: [{color: 'COLOR'}],
+                    fills: [{color: 'COLOR'}]
+                }
+            }]
+        }
+    ]
+};
+
+describe('color locator', () => {
+    describe('when searching on a page', () => {
+        let results;
+        beforeAll(() => {
+            results = findColorInstancesInPage('COLOR', page);
+        });
+
+        it('finds the color by page, artboard and layer', () => {
+            expect(results[0].page.name).toEqual('Page 1');
+            expect(results[0].artboard.name).toEqual('My artboard');
+            expect(results[0].layer.name).toEqual('second sub layer');
+        });
+
+        it("only returns relevant results, ignoring colors that don't match", () => {
+            expect(results.length).toBe(1);
+        });
     });
 
-    it('finds the color by page, artboard and layer', () => {
-        expect(results[0].page.name).toEqual('Page 1');
-        expect(results[0].artboard.name).toEqual('My artboard');
-        expect(results[0].layer.name).toEqual('second sub layer');
-    });
+    it("finds the color on borders", () => {
+        const results = findColorInstancesInPage('COLOR', pageWithBorderColor);
 
-    it("only returns relevant results, ignoring colors that don't match", () => {
         expect(results.length).toBe(1);
+        expect(results[0].page.name).toEqual('page');
+        expect(results[0].artboard.name).toEqual('artboard');
+        expect(results[0].layer.name).toEqual('layer with border');
     });
 });
+
 
 
